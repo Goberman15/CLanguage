e9540fcc92cef3456df6070f88822ac801ecec5d
goberman15 <akbarrmdhn94@gmail.com>
2023-01-08 22:07:40 +0700

    Add symbolic constant for start, step, and stop number

--- 1.Introduction/TempConversionForLoop.c
+++ 1.Introduction/TempConversionForLoop.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#define START 0
+#define STEP 20
+#define STOP 300
+
 int main(int argc, char const *argv[])
 {
-  for (int fahr = 0; fahr <= 300; fahr += 20)
+  for (int fahr = START; fahr <= STOP; fahr += STEP)
   {
     float celcius = (5.0 / 9.0) * (fahr - 32);
     printf("%3d %6.1f\n", fahr, celcius);
