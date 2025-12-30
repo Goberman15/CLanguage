af103ab7fcb6a11d279b5d69ec47481c45332fb7
Goberman15 <akbarrmdhn94@gmail.com>
2025-12-30 07:37:15 +0700

    Simple Wwc tool and power function

--- 1.Introduction/hello.c
+++ 1.Introduction/hello.c
@@ -2,7 +2,12 @@
 
 int main()
 {
-  printf("Hello!");
+  // before initialization, num holds indeterminate value
+  int num;
+  printf("Hello!\n");
+
+  num = 10;
+  printf("%d", num);
 
   return 0;
 }
